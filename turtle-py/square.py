c3cb2e3dc8dd5aed606f30d5b38727a422117036
Diego4Cornejo <diegocornejo15@gmail.com>
2018-06-03 23:12:25 -0400

    Squeare.py

---
+++ turtle-py/square.py
@@ -0,0 +1,14 @@
+import turtle
+
+windows = turtle.Screen()
+square = turtle.Turtle()
+square.forward(50)
+square.left(90)
+square.forward(50)
+square.left(90)
+square.forward(50)
+square.left(90)
+square.forward(50)
+square.left(90)
+
+turtle.mainloop()
